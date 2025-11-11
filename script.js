eb0fe58e0dc49ec948d3ebd920a66742cb04f98c
rradiohysteria@proton.me <rradiohysteria@proton.me>
2025-11-11 14:53:28 +0400

    add form validation

---
+++ script.js
@@ -0,0 +1,44 @@
+function validateInput(input, inputType, value) {
+    switch (inputType) {
+        case 'text':
+            if (value == "") {
+                const err_msg = document.getElementById(`${input.id}__err-msg`);
+                err_msg.classList.remove('hidden');
+            }
+            break;
+
+        case 'email':
+            const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+            if (!emailRegex.test(value)) {
+                const err_msg = document.getElementById(`${input.id}__err-msg`);
+                err_msg.classList.remove('hidden');
+            }
+            break;
+        
+        case 'password':
+            if (value == "") {
+                const err_msg = document.getElementById(`${input.id}__err-msg`);
+                err_msg.classList.remove('hidden');
+            }
+
+        default:
+            return true;
+    }
+}
+
+const form = document.getElementById('info-form');
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const inputs = ['firstname', 'lastname', 'useremail', 'userpassword'];
+
+    inputs.forEach((i) => {
+        //clear all error styling
+        const err_msg = document.getElementById(`${i}__err-msg`);
+        err_msg.classList.add('hidden');
+
+        const input = document.getElementById(i);
+        validateInput(input, input.type, input.value.trim());
+    })
+})
